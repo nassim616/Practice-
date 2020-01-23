2ef027991dce9f768371c3e308c26884bdc7f2a7
nassim <nassim.socio@gmail.com>
2020-01-22 21:45:34 -0600

    int added

--- src/main/java/Practice/FirstTestCase.java
+++ src/main/java/Practice/FirstTestCase.java
@@ -4,5 +4,9 @@ public class FirstTestCase {
     public static void main(String[] args) {
         System.out.println("nassim");
         System.out.println("moe");
+        int a = 2;
+        int b = 4 ;
+        System.out.println(a+b);
     }
 }
+
